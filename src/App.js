6f35dc4b754c2509e19516563a0e211479872cb7
IanHancock36 <northroadian@gmail.com>
2021-10-01 16:24:09 -0500

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./style.css";
+import axios from 'axios'
 
 export default function App() {
+  const [post, setPost] = React.useState('');
+  React.useEffect(() => {
+    axios.get('http://fortunecookieapi.com/v1/lessons/id')
+    .then((response) => {
+      setPost(response)
+      console.log(response.data)
+      console.log(post,'post')
+    .catch(error)
+    console.log(error)
+   });
+  }, []);
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+     <h1>{post.lessonId}</h1>
+      <p>{post.body}</p> 
     </div>
   );
 }
